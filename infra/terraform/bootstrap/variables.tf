f93febdb3c5dc1fe58d65bc4432e5152ee73c691
baedonguri <baedonguri@users.noreply.github.com>
2026-02-14 23:54:12 +0900

    fix: harden tfstate backend

---
+++ infra/terraform/bootstrap/variables.tf
@@ -0,0 +1,22 @@
+variable "aws_region" {
+  type    = string
+  default = "ap-northeast-2"
+}
+
+variable "state_bucket_name" {
+  type = string
+}
+
+variable "lock_table_name" {
+  type = string
+}
+
+variable "project_name" {
+  type    = string
+  default = "hello-my-oc"
+}
+
+variable "environment" {
+  type    = string
+  default = "personal"
+}
